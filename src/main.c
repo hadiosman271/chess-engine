b6d2403031eb16832938d8d7d5d583f5e832ccf6
hadiosman271 <hadiosman271@gmail.com>
2023-08-25 23:04:36 +0100

    move counters and en passant square are updated every move en passant and promotion work

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ void print_board(void);
 
 char *piece_types = "rnbqkp";
 char board[SIZE][SIZE];
-char turn = 'w';
+char turn;
 char ep_square[3];
 char castling_rights[5];
 int half_moves, full_moves;
@@ -23,12 +23,24 @@ int main(void)
     char *fen_startpos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     char *fen_kid = "r1bq1rk1/ppp1npbp/3p1np1/3Pp3/2P1P3/2N2N2/PP2BPPP/R1BQ1RK1 w - - 1 9";
     char *fen = "8/4p3/8/8/8/8/4P3/8";
+    char *moves[] = {
+        "e2e4", "a7a6", "e4e5", "d7d5", "e5d6", "a6a5",
+        "d6e7", "a5a4", "e7d8q", "a4a3", "d8e8", "a3b2",
+        "e8d8", "b2a1q"
+    };
+    char *moves2[] = {
+        "e2e4", "e7e5", "g1f3", "b8c6", "f1c4", "f8c5",
+        "c2c3", "g8f6"
+    };
 
     load_fen(fen_startpos);
-
-    print_board();
-    printf("%c %s %s %d %d\n",
-        turn, castling_rights, ep_square, half_moves, full_moves);
+    for (int i = 0; i < (sizeof(moves) / sizeof(moves[0])); i++)
+    {
+        make_move(moves[i]);
+        print_board();
+        printf("%c %s %s %d %d\n\n",
+            turn, castling_rights, ep_square, half_moves, full_moves);
+    }
 
     return 0;
 }
@@ -42,13 +54,48 @@ void make_move(char *move)
     }
 
     // Get the coordinates from the string
-    int rank_from = move[1] - '0' - 1;
-    int file_from = move[0] - 'a';
-    int rank_to = move[3] - '0' - 1;
-    int file_to = move[2] - 'a';
+    int rank_from, file_from, rank_to, file_to;
+    get_coords(move, &rank_from, &file_from, &rank_to, &file_to);
 
-    // Move the piece
-    board[rank_to][file_to] = board[rank_from][file_from];
+    // en passant
+    int rank_ep = ep_square[1] - '0' - 1;
+    int file_ep = ep_square[0] - 'a';
+    ep_square[0] = '-';
+    ep_square[1] = '\0';
+    if (tolower(board[rank_from][file_from]) == 'p')
+    {
+        if (rank_to == rank_ep && file_to == file_ep)
+        {
+            board[rank_to + (turn == 'w' ? -1 : 1)][file_to] = '\0';
+        }
+
+        if (abs(rank_to - rank_from) == 2)
+        {
+            ep_square[1] = rank_to + (turn == 'w' ? -1 : 1) + '0' + 1;
+            ep_square[0] = file_to + 'a';
+        }
+    }
+
+    // Update counters
+    if (turn == 'b')
+    {
+        full_moves++;
+    }
+    char target = board[rank_to][file_to];
+    // If a pawn is moved or a piece is captured
+    if (tolower(board[rank_from][file_from]) == 'p'
+        || ((target != '\0') && (turn == 'w' ? islower(target) : isupper(target))))
+    {
+        half_moves = 0;
+    }
+    else
+    {
+        half_moves++;
+    }
+
+    // Move the piece and promote if its a pawn on the 8th rank
+    board[rank_to][file_to] = move[4] == '\0' ? board[rank_from][file_from] :
+        (turn == 'w' ? toupper(move[4]) : move[4]);
     board[rank_from][file_from] = '\0';
 
     // Switch color
